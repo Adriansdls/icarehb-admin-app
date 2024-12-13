68f7fa6e9b14cf2cd13ce6508398a55f8ac47721
adriansdls <103926602+Adriansdls@users.noreply.github.com>
2024-12-13 10:32:17 +0100

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,9 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'ICArEHB Welcome Agent',
+  title: 'ICArEHB',
   description: "This agent is here to guide you and help you settle in, offering all the essential information you'll need as you start this new chapter with us.",
-  copyright: 'ICArEHB Welcome Agent 2024',
+  copyright: 'ICArEHB',
   privacy_policy: '',
   default_language: 'en',
 }
